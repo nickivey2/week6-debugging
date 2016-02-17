f5a5812c7b3a73848fc703762571b6a2510dc133
Nicholas Ivey <nickivey@Nicks-MacBook-Pro.local>
2016-02-17 15:07:05 -0500

    Edit

--- calculator.py
+++ calculator.py
@@ -23,6 +23,6 @@ def divide( first, second):
     #       I'm sorry, I can't divide by zero
     #   3. returns the quotient of first and second
     if second == 0:
-        print("I'm sorry, I can't divide by zero")
+        raise Exception("I'm sorry, I can't divide by zero")
     else:
         return first/second
